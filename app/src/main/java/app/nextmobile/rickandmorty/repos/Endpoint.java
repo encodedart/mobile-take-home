8a01fa3b1d095d13fb08124821a802acc5ed376c
Kim N <kevin@encodedart.com>
2019-08-29 08:37:59 -0400

    Add characters endpoint

--- app/src/main/java/app/nextmobile/rickandmorty/repos/Endpoint.java
+++ app/src/main/java/app/nextmobile/rickandmorty/repos/Endpoint.java
@@ -2,4 +2,5 @@ package app.nextmobile.rickandmorty.repos;
 
 public class Endpoint {
     public static String episodes = "https://rickandmortyapi.com/api/episode/";
+    public static String character = "https://rickandmortyapi.com/api/character/";
 }
